cc0e86006ec8a5c26892f0e0f1809ce531b833a3
Kevin J. Kircher <kevin.j.kircher@gmail.com>
2024-01-29 12:47:28 -0500

    Update simulateEV.m

--- electric-vehicles/simulateEV.m
+++ electric-vehicles/simulateEV.m
@@ -1,5 +1,36 @@
 %% introduction
 % This script simulates three charging policies for an electric vehicle.
+%
+% Kevin J. Kircher, Purdue University, 2024
+%
+% Please make sure the following functions are on your Matlab path:
+%   fullStairs.m (stairstep plots)
+%   plotEVresults.m (results plots)
+%   generateDrivingPower.m (generates plausible EV power for driving)
+%   simulatePolicy1.m (students fill this in)
+%   simulatePolicy2.m (students fill this in)
+%   simulatePolicy3.m (students fill this in)
+
+%% graphics settings
+% I recommend adding these to your startup.m file. Matlab runs that file at
+% startup, effectively turning these settings into new defaults.
+
+% set LaTeX as the default text interpreter
+set(0,'DefaultTextInterpreter','latex');
+set(0,'DefaultLegendInterpreter','latex');
+set(0,'DefaultAxesTickLabelInterpreter','latex');
+
+% set the default font size for axes ticks/labels and figure titles
+set(0,'DefaultAxesFontSize',24);
+
+% set the default text size for figures and legends
+set(0,'DefaultTextFontSize',24);
+set(0,'DefaultLegendFontSizeMode','manual')
+set(0,'DefaultLegendFontSize',24)
+
+% set the default line width for plots and stairstep plots
+set(0,'DefaultLineLineWidth',2.5);
+set(0,'DefaultStairLineWidth',2.5);
 
 %% data
 % timing
