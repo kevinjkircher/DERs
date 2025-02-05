9a9fc637d3b5da09b838b2743edacae2c28afe44
Kevin J. Kircher <kevin.j.kircher@gmail.com>
2025-02-05 08:34:15 -0500

    Update simulate2R2C.py
    Adjusted solar component of exogenous thermal power

--- buildings/simulate2R2C.py
+++ buildings/simulate2R2C.py
@@ -97,9 +97,12 @@ t_elec = t_span.map(lambda x: x.replace(year=2019))
 plug_powers = import_electricity(electricity_file, t_elec)
 
 # Exogenous thermal power
+lam = 0.25 % glazing ratio
+c = 0.4 % solar heat gain coefficient
 qe = (plug_powers[:, np.random.randint(plug_powers.shape[1])] +  # from plugged-in devices
-      0.19 * np.sqrt(N * Af) * I +  # from the sun
+      4.8 * c * lam * np.sqrt(N * Af * I) +  # from the sun
       1 + (0.5 / 3) * np.random.randn(K))  # from everything else
+
 # Disturbance
 w = qe + Tout / R  # kW
 
